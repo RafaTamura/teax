5ff34a8b974ef8d74d11117569e419396a613c0f
Rafa Tamura <rafaelatamura@gmail.com>
2023-01-25 02:22:18 -0300

    esqueleto site

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import './App.css';
-import {  } from 'reactstrap';
+import { Col, Container, Row } from 'reactstrap';
 
 function App() {
   return (
-    
+
     <div className="App">
       <header className="App-header">
-        <div > 
+        <div className='cabeca'>
+          head
         </div>
+        <Container fluid>
+          <Row>
+            <Col xs="2" className="esqlateral">
+            primeira parte
+            </Col>
+            <Col xs="8" className='conteudo'>
+              conteudo
+            </Col>
+            <Col xs="2" className='dirlateral'>
+              lembretes 
+            </Col>
+            </Row>
+        </Container>
       </header>
     </div>
   );
