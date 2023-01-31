9bdab9386b52eab7195cbe549df63ec01b6bc0f0
Rafa Tamura <rafaelatamura@gmail.com>
2023-01-31 10:08:34 -0300

    ajuste na barra esquerda

--- src/App.js
+++ src/App.js
@@ -18,12 +18,21 @@ function App() {
             <Col xs="2" className="esqlateral">
             <div className='perfil'>
             </div>
-            <div>
+            <div className='nome'>
               Hey, {Nome}
+            </div>
+            <div className='bar'>
+            </div>
+            <div className='barralateral'>
+              
+              <a href='App.js'> Perfil</a>
+              <a href='App.js'> Rotina</a>
+              <a href='App.js'> Atividades</a>
+              <a href='App.js'> Amigos</a>
             </div>
             </Col>
             <Col xs="8" className='conteudo'>
-              conteudo
+            conteudo
             </Col>
             <Col xs="2" className='dirlateral'>
               lembretes 
